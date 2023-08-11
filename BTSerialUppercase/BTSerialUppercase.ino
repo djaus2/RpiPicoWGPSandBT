d7dce5b280cb0323f3ebe99e5601508f7c1a1cd3
djaus2 <davidjones@sportronics.com.au>
2023-08-11 21:12:55 +1000

    Added BTSerialUppercase

---
+++ BTSerialUppercase/BTSerialUppercase.ino
@@ -0,0 +1,44 @@
+// A very simple Serial-over-BT app that reads input from the host and CAPITALIZES it.
+// Released to the public domain by Earle F. Philhower, III, in February 2023
+
+// Under Linux to connect to the PicoW
+// 1. Pair to the "PicoW Serial XX:XX..." device using your favorite GUI, entering a PIN of "0000"
+// 2. Execute "sudo rfcomm bind 0 00:00:00:00:00:00" to make a `/dev/rfcomm0" device, replacing the "00:00.." with the MAC as listed in the device name
+// 3. Run "minicom -D /dev/rfcomm0" and type away
+// 4. To remove the virtual serial port, execute "sudo rfcomm release rfcomm0"
+
+// Under Windows to connect to the PicoW
+// 1. Pair to the "PicoW Serial XX:XX..." device using the copntrol panel, ignoring any PIN it says to check for
+// 2. At this point you will have a new COM: port.  You may need to use the Device Manager to find it's number.
+// 3. Open up COMX: in your favorite terminal application and type away
+
+// Under Mac to connect to the PicoW
+// 1. Open System Preferences and go in the bluetooth section. You should find a bluetooth device called
+//    PicoW Serial XX:XX:... Click Connect button.
+// 2. A /dev/tty.PicoWSerialXXXX becomes available.
+// 3. Connect to this device with your favorite terminal application.
+
+// Enabling Bluetooth
+// To enable Bluetooth (BT), use the Tools->IP/Bluetooth Stack menu.
+
+
+#include <SerialBT.h>
+
+void setup() {
+  Serial.begin(9600);
+  SerialBT.begin();
+  delay(1000);
+  Serial.println("Start");
+  Serial.println(SerialBT.availableForWrite());
+}
+
+void loop() {
+  while (SerialBT) {
+    while (SerialBT.available()) {
+      char c = SerialBT.read();
+      Serial.write(c);
+      c = toupper(c);
+      SerialBT.write(c);
+    }
+  }
+}
